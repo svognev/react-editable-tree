a7aa0a8080a86723264a9a1bd5a7d965e21e3cb0
Svetozar Ognev <svognev@yandex.com>
2019-04-14 02:37:40 +0300

    Updates

---
+++ precache-manifest.889b272d937950e0f9e5fe5482dc35c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f1813bb2fedbb5746db",
+    "url": "/react-editable-tree/static/js/runtime~main.4c8466e7.js"
+  },
+  {
+    "revision": "9fc4dcef4261bc9390df",
+    "url": "/react-editable-tree/static/js/main.07253f9d.chunk.js"
+  },
+  {
+    "revision": "6c7625d27564d4430502",
+    "url": "/react-editable-tree/static/js/2.b60023d4.chunk.js"
+  },
+  {
+    "revision": "9fc4dcef4261bc9390df",
+    "url": "/react-editable-tree/static/css/main.8711fa87.chunk.css"
+  },
+  {
+    "revision": "29c3c95f2ea2a5426950a76b7175b466",
+    "url": "/react-editable-tree/index.html"
+  }
+];
